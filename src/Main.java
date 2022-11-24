2be077f19c83e09f416f0655b017ad8f2bce8782
Kirill Morozov <kmmorozov@gmail.com>
2022-11-24 18:59:15 +0300

    первый пример

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] ar1 = {14, 21, 56, 81, 12};
+        int size=7;
+        int[] ar2 = new int[size];
+       //ввод массива из консоли
+        Scanner scanner=new Scanner(System.in);
+        for (int i = 0; i < size; i++) {
+            System.out.println("введите число");
+            ar2[i] = scanner.nextInt();
+        }
+
+        //вывод массива поэлементно в консоль
+        for (int i = 0; i < size; i++) {
+            System.out.println(ar2[i]);
+        }
     }
 }
