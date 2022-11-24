5837eefe29608021a4e7a235031114c16c7fbc9a
Kirill Morozov <kmmorozov@gmail.com>
2022-11-24 19:12:30 +0300

    первый пример с изменением

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    int[] ar1 = {14, 21, 56, 81, 12};
-        int size=7;
-        int[] ar2 = new int[size];
+        //int size=7;
+        int[] ar2 = new int[7];
        //ввод массива из консоли
         Scanner scanner=new Scanner(System.in);
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < ar2.length; i++) {
             System.out.println("введите число");
             ar2[i] = scanner.nextInt();
         }
 
         //вывод массива поэлементно в консоль
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < ar2.length ; i++) {
             System.out.println(ar2[i]);
         }
     }
